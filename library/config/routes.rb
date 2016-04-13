4ebd3b00c5925aa572e009acc8fff180c49dcd59
tuckerbohman5 <tucker.bohman@gmail.com>
2016-04-13 08:56:30 -0600

    initialized angular using ng-app in application layout, still need to create app module

--- library/config/routes.rb
+++ library/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  root "application#index"
+
+  
   namespace :api, defaults:{format: :json} do
     namespace :v1 do
       resources :teachers, :books, :authors, :grades, :reading_levels, :schools
