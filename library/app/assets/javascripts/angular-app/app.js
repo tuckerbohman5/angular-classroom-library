ea954e90ed78b852189308352ccfbff96b8fa054
tuckerbohman5 <tucker.bohman@gmail.com>
2016-04-13 10:43:38 -0600

    added config to app.js with states

--- library/app/assets/javascripts/angular-app/app.js
+++ library/app/assets/javascripts/angular-app/app.js
@@ -1,2 +1,21 @@
-angular 
-  .module('app', ['ui.router', 'ngResource']);
+angular
+ .module('app', ['ui.router', 'ngResource'])
+ .config(function($stateProvider, $urlRouterProvider) {
+   $stateProvider
+     .state('home', {
+       url: '/',
+       templateUrl: 'home.html',
+       controller: 'HomeController as ctrl'
+     })
+     .state('home.new', {
+       url: 'new',
+       templateUrl: 'home/new.html',
+       controller: 'BooksController as ctrl'
+     })
+     .state('home.books', {
+       url: 'books',
+       templateUrl: 'home/books.html',
+       controller: 'BooksController as ctrl'
+     });
+  $urlRouterProvider.otherwise('/');
+});
