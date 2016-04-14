3ed7e9cb0717cbb023cd6c5d00d803853dd827f8
tuckerbohman5 <tucker.bohman@gmail.com>
2016-04-13 19:08:44 -0600

    created custom as_json method for books to allow for associations

--- library/app/models/book.rb
+++ library/app/models/book.rb
@@ -2,4 +2,9 @@ class Book < ActiveRecord::Base
   belongs_to :author
   belongs_to :reading_level
   belongs_to :teacher
+
+
+  def as_json(options = {})
+    super(options.merge(include: [:author, :reading_level, :teacher]))
+  end
 end
