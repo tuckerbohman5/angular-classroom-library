67394c719688d870272d8fdae6d3e1ed7de84d3c
tuckerbohman5 <tucker.bohman@gmail.com>
2016-04-15 11:34:23 -0600

    my library is not loading any content from the controller or model

--- library/app/assets/javascripts/angular-app/controllers/LibraryController.js
+++ library/app/assets/javascripts/angular-app/controllers/LibraryController.js
@@ -2,8 +2,8 @@ angular
   .module('app')
   .controller('LibraryController', LibraryController);
 
-function LibraryController(Auth) {
+function LibraryController(Auth, User) {
   var ctrl = this;
-  ctrl.user = Auth.currentUser();
+  ctrl.user = User.get(Auth.currentUser());
   ctrl.books = ctrl.user.books
 };
