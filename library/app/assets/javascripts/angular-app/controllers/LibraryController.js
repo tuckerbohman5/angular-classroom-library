f76b8d8ae99b321a3ed2a8f516987b796c745d5a
tuckerbohman5 <tucker.bohman@gmail.com>
2016-04-16 10:32:45 -0600

    still not working

--- library/app/assets/javascripts/angular-app/controllers/LibraryController.js
+++ library/app/assets/javascripts/angular-app/controllers/LibraryController.js
@@ -4,7 +4,7 @@ angular
 
 function LibraryController(Book, $filter) {
   var ctrl = this;
-  ctrl.books = Book.query({user_id:1});
+  ctrl.books = Book.query([user_id: 2]);
 
   // ctrl.library = $filter('filter')(ctrl.books, user_id:2);
   
