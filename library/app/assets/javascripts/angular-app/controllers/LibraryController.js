29f64ef4ba593cfb0ed1d4b56c2c75a4e73a23b9
tuckerbohman5 <tucker.bohman@gmail.com>
2016-04-16 10:22:01 -0600

    something is still not working right with devise or the library controller, it is still returning all books

--- library/app/assets/javascripts/angular-app/controllers/LibraryController.js
+++ library/app/assets/javascripts/angular-app/controllers/LibraryController.js
@@ -2,9 +2,11 @@ angular
   .module('app')
   .controller('LibraryController', LibraryController);
 
-function LibraryController(Auth, User) {
+function LibraryController(Book, $filter) {
   var ctrl = this;
-  ctrl.user = Auth.currentUser();
-  debugger;
+  ctrl.books = Book.query({user_id:1});
+
+  // ctrl.library = $filter('filter')(ctrl.books, user_id:2);
+  
   // ctrl.books = Book.query(id: ctrl.user);
 };
