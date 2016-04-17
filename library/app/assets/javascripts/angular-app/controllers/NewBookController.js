ea4f817908ef1a6feb1d0a4c3c1d20208eb240c7
tuckerbohman5 <tucker.bohman@gmail.com>
2016-04-16 19:26:06 -0600

    issue was with typo in ctr which should be ctrl

--- library/app/assets/javascripts/angular-app/controllers/NewBookController.js
+++ library/app/assets/javascripts/angular-app/controllers/NewBookController.js
@@ -1,10 +1,11 @@
 angular 
   .module('app') 
   .controller('NewBookController', NewBookController); 
-function NewBookController(Book, $location) {
+function NewBookController(Book, $location, Author) {
   // we will use $location to route back to another state
   var ctrl = this; 
   // make a new note 
+  ctrl.authors = Author.query();
   ctrl.book = new Book();
   ctrl.addBook = function() {
     ctrl.book.$save(function() {
