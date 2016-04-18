e37d0ae1aa3b06bcac7634c5074ca2c505dc970c
tuckerbohman5 <tucker.bohman@gmail.com>
2016-04-18 00:08:44 -0600

    teacher show page now shows their books

--- library/app/assets/javascripts/angular-app/controllers/ShowTeacherController.js
+++ library/app/assets/javascripts/angular-app/controllers/ShowTeacherController.js
@@ -1,8 +1,9 @@
 angular
  .module('app')
  .controller('ShowTeacherController', ShowTeacherController);
-function ShowTeacherController(User, $stateParams) {
+function ShowTeacherController(User, $stateParams, Book) {
    
   var ctrl = this;
+  ctrl.books = Book.query();
   ctrl.user = User.get({ id: $stateParams.id });
 }
