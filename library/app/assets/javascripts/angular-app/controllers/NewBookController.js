da6c9eee342f8d1bcc2aa47e8ec4973d296ce710
tuckerbohman5 <tucker.bohman@gmail.com>
2016-04-17 21:10:45 -0600

    new book form creation still working but changes to add current user are not working. User id shows blank in new book

--- library/app/assets/javascripts/angular-app/controllers/NewBookController.js
+++ library/app/assets/javascripts/angular-app/controllers/NewBookController.js
@@ -1,10 +1,14 @@
 angular 
   .module('app') 
   .controller('NewBookController', NewBookController); 
-function NewBookController(Book, $location, Author, ReadingLevel) {
+function NewBookController(Book, $location, Author, ReadingLevel, Auth) {
   // we will use $location to route back to another state
   var ctrl = this; 
   // make a new note 
+  Auth.currentUser()
+    .then(function(user) {
+      ctrl.user = user;
+    });
   ctrl.readingLevels = ReadingLevel.query();
   ctrl.authors = Author.query();
   ctrl.book = new Book();
