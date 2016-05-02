e1debc2c990812faa8ba980b85d1b0eab909e89a
tuckerbohman5 <tucker.bohman@gmail.com>
2016-05-01 20:17:45 -0600

    created function for return book need to add attribute to model

--- library/app/assets/javascripts/angular-app/components/ReceivedRequest.js
+++ library/app/assets/javascripts/angular-app/components/ReceivedRequest.js
@@ -20,6 +20,14 @@ var ReceivedRequest = {
        
       })    
     }
+    //I need to add an attribute to request: returned/boolean
+    ctrl.returnBook = function(){
+      ctrl.request.returned = true;
+
+      Request.update(ctrl.request, function(resp){
+        
+      })
+    }
   },
     
   controllerAs: 'ctrl'
